d765e532e5db6d60ba665c28c89504c0e8955f9a
moljac <mcvjetko@holisticware.net>
2024-02-01 15:00:25 +0100

    newer sample 20240131

---
+++ samples/javier/20240131/ClientAppsIntegration.MAUI/ClientAppsIntegration.AppHost/Program.cs
@@ -0,0 +1,18 @@
+﻿using HolisticWare.Aspire.Hosting.Maui;
+
+HolisticWare.Tools.Devices.Android.Emulator.Launch("nexus_9_api_33");
+HolisticWare.Tools.Devices.Android.Emulator.Launch("Pixel_3a_API_34_extension_level_7_arm64-v8a");
+    
+System.Threading.Thread.Sleep(10000);
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+var apiService = builder.AddProject<Projects.ClientAppsIntegration_ApiService>("apiservice");
+
+// Register the client apps by project path as they target a TFM incompatible with the AppHost so can't be added as
+// regular project references (see the AppHost.csproj file for additional metadata added to the ProjectReference to
+// coordinate a build dependency though).
+builder.AddProject("mauiclient", "../ClientAppsIntegration.MAUI/ClientAppsIntegration.MAUI.csproj")
+   .WithReference(apiService);
+
+builder.Build().Run();
